3779535a731f387864340add097f7ba6cd2fb0e3
Hector Gomez <hector.gomez.varela@gmail.com>
2022-04-17 11:44:38 +0200

    [0.1.0] init web server

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+require('newrelic');
+
+const fastify = require('fastify')({
+  logger: true
+});
+
+const { HTTP_SERVER_PORT } = process.env;
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(HTTP_SERVER_PORT)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+};
+
+start();
