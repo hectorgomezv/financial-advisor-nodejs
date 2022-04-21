d2ac21453b39d4d6b36196b387186adf27ca258d
Hector Gomez <hgomez@newrelic.com>
2022-04-21 23:29:41 +0200

    [0.7.0] Install Jest

---
+++ src/__tests__/unit/positions/domain/use-cases/get-positions-uc.js
@@ -0,0 +1,7 @@
+const { getPositions } = require('../../../../../app/positions/domain/use-cases');
+const { positionsRepository } = require('../../../../../app/positions/domain/repositories');
+const { companiesRepository, companyStatesRepository } = require('../../../../../app/companies/domain/repositories');
+
+test('foo', () => {
+  expect(2).toBe(1 + 1);
+});
