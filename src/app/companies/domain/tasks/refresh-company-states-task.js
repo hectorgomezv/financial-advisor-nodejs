baa5c020f779e0a7d25e7b0a3099c9cd5678caf5
Hector Gomez <hgomez@newrelic.com>
2022-04-30 20:44:51 +0200

    Add cron expressions for retrieving company states

--- src/app/companies/domain/tasks/refresh-company-states-task.js
+++ src/app/companies/domain/tasks/refresh-company-states-task.js
@@ -1,7 +1,9 @@
+const { CronJob } = require('cron');
 const { CompaniesRepository, CompanyStatesRepository } = require('../repositories');
 
 const FIVE_SECONDS_MS = 5 * 1000;
-const TWELVE_HOURS_MS = 12 * 60 * 60 * 1000;
+const MARKET_OPEN_CRON_EXP = '0 32 15 * * *';
+const MARKET_CLOSE_CRON_EXP = '0 02 22 * * *';
 
 const refreshCompanyStates = async () => {
   const companies = await CompaniesRepository.find();
@@ -10,7 +12,13 @@ const refreshCompanyStates = async () => {
 
 const run = () => {
   setTimeout(() => refreshCompanyStates(), FIVE_SECONDS_MS);
-  setInterval(() => refreshCompanyStates(), TWELVE_HOURS_MS);
+
+  const jobs = [
+    new CronJob(MARKET_OPEN_CRON_EXP, () => refreshCompanyStates(), null, false, 'Europe/Madrid'),
+    new CronJob(MARKET_CLOSE_CRON_EXP, () => refreshCompanyStates(), null, false, 'Europe/Madrid'),
+  ];
+
+  jobs.map(j => j.start());
 };
 
 module.exports = { run };
