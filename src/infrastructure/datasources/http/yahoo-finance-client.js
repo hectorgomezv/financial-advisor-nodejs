4203c7514e4a44c3ade9b2654b1e8e6080117aa5
Hector Gomez <hgomez@newrelic.com>
2022-05-05 00:26:04 +0200

    Change datasource to yahoo finance

--- src/infrastructure/datasources/http/yahoo-finance-client.js
+++ src/infrastructure/datasources/http/yahoo-finance-client.js
@@ -8,15 +8,18 @@ const mapCompanyStateResponse = data => ({
   peg: Number(data.defaultKeyStatistics.pegRatio.raw),
 });
 
-const { PROVIDER_BASE_URL } = process.env;
+const { PROVIDER_API_TOKEN, PROVIDER_BASE_URL } = process.env;
 
 const BASE_URL = `${PROVIDER_BASE_URL}/finance/quoteSummary`;
 const DEFAULT_MODULES = '?modules=summaryDetail,defaultKeyStatistics,fundOwnership,majorDirectHolders';
 
 const getQuoteSummary = async symbol => {
-  const { data } = await axios.get(`${BASE_URL}/${symbol}${DEFAULT_MODULES}`);
+  const { data } = await axios.get(
+    `${BASE_URL}/${symbol}${DEFAULT_MODULES}`,
+    { headers: { 'x-api-key': PROVIDER_API_TOKEN } },
+  );
 
-  return mapCompanyStateResponse(data);
+  return mapCompanyStateResponse(data.quoteSummary.result[0]);
 };
 
 module.exports = { getQuoteSummary };
