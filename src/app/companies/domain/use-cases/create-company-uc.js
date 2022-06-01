ca64b9535f30feae4248f6a2b12b8d4c51b81bb0
Hector Gomez <hgomez@newrelic.com>
2022-06-01 13:23:21 +0200

    fix error on company creation

--- src/app/companies/domain/use-cases/create-company-uc.js
+++ src/app/companies/domain/use-cases/create-company-uc.js
@@ -1,10 +1,9 @@
-const { CompaniesService, CompanyStatesService } = require('../services');
+const { CompaniesService } = require('../services');
 const { RbacService } = require('../../../shared/domain/services');
 
 module.exports = async (context, input) => {
   await RbacService.isUserAllowedTo(context, 'create', 'company');
   const company = await CompaniesService.createCompany(input);
-  await CompanyStatesService.refreshCompanyState(company);
 
   return company;
 };
