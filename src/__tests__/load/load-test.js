b95134ed3b70747f777f81b2b74e9f9380faefe4
Hector Gomez <hgomez@newrelic.com>
2022-06-05 16:12:11 +0200

    continue load test

--- src/__tests__/load/load-test.js
+++ src/__tests__/load/load-test.js
@@ -42,7 +42,6 @@ export default function run() {
     },
   };
 
-  const res = http.get(`${baseUrl}/companies`, params);
-  console.log(JSON.stringify(res));
+  http.get(`${baseUrl}/companies`, params);
   sleep(0.2);
 }
