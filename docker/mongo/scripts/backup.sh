b99d446b7704f3d625b83e73fd232c1308f1e75a
Hector Gomez <hector.gomez.varela@gmail.com>
2022-06-10 22:27:28 +0000

    update mongo backup script

--- docker/mongo/scripts/backup.sh
+++ docker/mongo/scripts/backup.sh
@@ -9,6 +9,6 @@ BACKUP_NAME=$DB-$(date +%y%m%d_%H%M%S).gz
 date
 
 echo "Dumping MongoDB $DB database to compressed archive"
-docker exec financial-advisor-nodejs-fa-mongo-1 mongodump --db $DB --archive=$HOME/backup/$BACKUP_NAME --gzip -u admin --authenticationDatabase=admin -p $ADMIN_PASSWORD 
-docker cp financial-advisor-nodejs-fa-mongo-1:/root/backup/ /root/backup/
+docker exec financial-advisor-nodejs-fa-mongo-1 mongodump --db $DB --archive=$HOME/$BACKUP_NAME --gzip -u admin --authenticationDatabase=admin -p $ADMIN_PASSWORD 
+docker cp financial-advisor-nodejs-fa-mongo-1:/$HOME/$BACKUP_NAME /root/backup/
 
