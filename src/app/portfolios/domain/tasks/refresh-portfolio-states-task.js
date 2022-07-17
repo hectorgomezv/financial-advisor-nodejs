bce32088e1a345d913196bd41962f6bfcf5c368f
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-07-17 16:14:16 +0200

    Refresh portfolio states on market open and close only

--- src/app/portfolios/domain/tasks/refresh-portfolio-states-task.js
+++ src/app/portfolios/domain/tasks/refresh-portfolio-states-task.js
@@ -2,7 +2,8 @@ const { CronJob } = require('cron');
 const { PortfoliosService, PortfolioStatesService, PositionsService } = require('../services');
 
 const TEN_SECONDS_MS = 10 * 1000;
-const EVERY_HOUR_CRON_EXP = '0 * * * *';
+const MARKET_OPEN_CRON_EXP = '0 34 15 * * *';
+const MARKET_CLOSE_CRON_EXP = '0 04 22 * * *';
 
 const refreshPortfolioStates = async () => {
   const portfolios = await PortfoliosService.getAll();
@@ -15,8 +16,13 @@ const refreshPortfolioStates = async () => {
 
 const run = () => {
   setTimeout(() => refreshPortfolioStates(), TEN_SECONDS_MS);
-  const job = new CronJob(EVERY_HOUR_CRON_EXP, () => refreshPortfolioStates());
-  job.start();
+
+  const jobs = [
+    new CronJob(MARKET_OPEN_CRON_EXP, () => refreshPortfolioStates(), null, false, 'Europe/Madrid'),
+    new CronJob(MARKET_CLOSE_CRON_EXP, () => refreshPortfolioStates(), null, false, 'Europe/Madrid'),
+  ];
+
+  jobs.map(j => j.start());
 };
 
 module.exports = { run };
